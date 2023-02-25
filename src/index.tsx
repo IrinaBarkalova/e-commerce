25eb080263f7e48c4adeda82bc5f20a4b74a28d6
IrinaBarkalova <i_barkalova@mail.ru>
2023-02-26 00:40:40 +0300

    init dev

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,4 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode> </React.StrictMode>);
