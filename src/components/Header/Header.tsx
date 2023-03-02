e143fa781a991875d7c89311c3edf0067cb64a95
IrinaBarkalova <i_barkalova@mail.ru>
2023-03-02 22:56:09 +0300

    add header for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,15 @@
 import { memo } from 'react';
 import React from 'react';
 
-import Icon from '@components/Header/Components/Label';
-import bag from '@components/img/bag.svg';
-import label from '@components/img/label.svg';
-import user from '@components/img/user.svg';
-import { Link } from 'react-router-dom';
-
-import styles from './Header.module.scss';
+import DesktopHeader from '@components/Header/Components/DesktopHeader';
+import MobileHeader from '@components/Header/Components/MobileHeader';
 
 const Header: React.FC = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.header__icon}>
-        <Icon src={label} />
-      </div>
-      <div className={styles.header__items}>
-        <li className={styles.header__item}>
-          <Link className={styles.header__link} to="/">
-            Products
-          </Link>
-        </li>
-        <li className={styles.header__item}>
-          <Link className={styles.header__link} to="/product/1">
-            Categories
-          </Link>
-        </li>
-        <li className={styles.header__item}>
-          <Link className={styles.header__link} to="/">
-            About Us
-          </Link>
-        </li>
-      </div>
-      <div className={styles.header__icon}>
-        <Icon style={{ marginRight: '22px' }} src={bag} />
-        <Icon src={user} />
-      </div>
-    </div>
+    <>
+      <DesktopHeader />
+      <MobileHeader />
+    </>
   );
 };
 
